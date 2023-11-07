26c5e88af700d3195f396b1848580d66bddb789d
kingofnorth1 <3441423164@qq.com>
2023-11-08 00:46:53 +0800

    MySpider项目，爬取douban网站,遍历所有页面

--- pythonSpider/pythonScrapy/MySpider/MySpider/spiders/douban.py
+++ pythonSpider/pythonScrapy/MySpider/MySpider/spiders/douban.py
@@ -1,5 +1,6 @@
 import scrapy
-from scrapy import Selector
+from scrapy import Selector, Request
+from scrapy.http import HtmlResponse
 
 from ..items import MovieItem
 
@@ -7,14 +8,23 @@ from ..items import MovieItem
 class DoubanSpider(scrapy.Spider):
     name = "douban"
     allowed_domains = ["movie.douban.com"]
-    start_urls = ["https://movie.douban.com/top250"]
+    # start_urls = ["https://movie.douban.com/top250?start=0&filter="]
 
-    def parse(self, response):
+    def start_requests(self):
+        for page in range(10):
+            yield Request(url=f'https://movie.douban.com/top250?start={page * 25}&filter=')
+
+    def parse(self, response: HtmlResponse, **kwargs):
         sel = Selector(response)
-        list_items = sel.css('html.ua-windows.ua-ff11 body div#wrapper div#content div.grid-16-8.clearfix div.article ol.grid_view li')
+        list_items = sel.css('#content > div > div.article > ol > li')
         for list_item in list_items:
             movieItem = MovieItem()
             movieItem['title'] = list_item.css('span.title::text').extract_first()
             movieItem['rank'] = list_item.css('span.rating_num::text').extract_first()
             movieItem['subject'] = list_item.css('span.inq::text').extract_first()
             yield movieItem
+
+        # hrefs_list = sel.css('div.paginator > a::attr(href)')
+        # for href in hrefs_list:
+        #     url = response.urljoin(href.extract())
+        #     yield Request(url=url)
